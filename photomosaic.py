d00e0fc5973388aaaae9a54848ca8ca7e9044c66
zAle711 <alessandro7111@gmail.com>
2021-12-20 14:54:55 +0100

    added a method to color block by average pixels

--- photomosaic.py
+++ photomosaic.py
@@ -4,24 +4,57 @@ import math
 import random
 
 
-def editImage(image):
+
+def randomPixels(image, size):
+    """Replace square of pixels with sixe x size area with a radom color
+
+    Args:
+        image (PIL.Image): original image
+        size (int): size of every random color
+    """
+    
     pixels = np.array(image)
-    common_divosor = math.gcd(image.height, image.width)
-    COLUMN_LIMIT = image.width / common_divosor
+    COLUMN_LIMIT = (image.width / size) - 1
+    ROW_LIMIT = image.height / size
     x = 0
     y = 0
-    while y < image.height/common_divosor :
+    while y < ROW_LIMIT :
         rgb = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
-        row = y * 120
-        column = x * 120
-        pixels[row: row + common_divosor, column : column + common_divosor] = rgb
+        row = y * size
+        column = x * size
+        pixels[row: row + size, column : column + size] = rgb
         if x < COLUMN_LIMIT:
             x += 1
         else:
             x = 0
             y += 1
-    
     showImage(pixels)
+
+def getBlockPixels(image, size):
+    pixels = np.array(image)
+    COLUMN_LIMIT = (image.width / size) - 1
+    ROW_LIMIT = image.height / size
+    x = 0
+    y = 0
+    avg_blocks = np.array([])
+    while y < ROW_LIMIT :
+        pixels_average = np.zeros(3)
+        rgb = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
+        row = y * size
+        column = x * size
+        block = pixels[row: (row + size) , column : (column + size) ]
+        for b in block:
+           for p in b:
+               pixels_average += p
+        pixels_average /= block.size
+        block = pixels[row: (row + size) , column : (column + size) ] = pixels_average.astype(int)
+        if x < COLUMN_LIMIT:
+            x += 1
+        else:
+            x = 0
+            y += 1
+    showImage(pixels)
+    return(avg_blocks)
 
 
 def showImage(image_pixels):
@@ -32,6 +65,6 @@ def showImage(image_pixels):
 if __name__ == "__main__":
     main_image = Image.open("windows.jpg")
     print(f"Grandezza dell'immagine: {main_image.height} x {main_image.width}")    
-    editImage(main_image)
-    
+    randomPixels(main_image, 30)
+    print(getBlockPixels(main_image, 30))
 
